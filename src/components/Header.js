8363506c402618c225c8e7fc86c36557932a3f39
morpho-tom <achuk233@gmail.com>
2023-03-02 23:16:38 +0530

    icon setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Header.css';
 import Logo from "./images/Amazon_logo_PNG3.png";
-
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 function Header() {
     return (
         <div className='header'>
@@ -11,9 +12,22 @@ function Header() {
             </div>
             {/* search bar */}
             <div>
-                <input type='Text' placeholder='Search Items or products' /> 
+                <input type='Text' placeholder='Search Items or products' />
+                <SearchIcon />
             </div>
-            
+            <div>
+                <h4>Hello Morpho </h4>
+                <h4>Accounts & Lists</h4>
+            </div>
+            <div>
+                <h4>Returns </h4>
+                <h4>& Orders</h4>
+            </div>
+            <div style={{ position: "relative" }}>
+                <ShoppingCartOutlinedIcon style={{ color: "white" }} />
+                <span style={{ position: "absolute", left: 14, right: 14, backgroundColor: "white", width: 14, height: 14, borderRadius: 7, alignItems: "center", fontSize:12 ,fontWeight:400 }}>0</span>
+            </div>
+
         </div>
     )
 }
